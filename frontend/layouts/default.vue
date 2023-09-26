b0738cf1b243a8945006217ca90f6c284602d091
AirFast <airfast.88@gmail.com>
2023-09-26 21:21:13 +0300

    feat: add toggle dark btn

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,10 +1,19 @@
+<script setup lang="ts">
+import { MoonIcon, SunIcon } from "@heroicons/vue/24/solid";
+
+const { isDark, toggleDark } = useTheme();
+</script>
+
 <template>
   <div>
     Some default layout shared across all pages
     <slot />
 
     <div>
-      <ToggleDarkButton />
+      <TheButton @click="toggleDark()">
+        <MoonIcon v-if="!isDark" class="w-6 h-6 text-indigo-500" />
+        <SunIcon v-else class="w-6 h-6 dark:text-indigo-200" />
+      </TheButton>
     </div>
   </div>
 </template>
