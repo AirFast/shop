6d3561a15427d312e55cbfaaf18b45cf2f821ee6
AirFast <airfast.88@gmail.com>
2023-09-26 21:03:19 +0300

    feat: add dark mode

---
+++ frontend/components/ToggleDarkButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { MoonIcon, SunIcon } from "@heroicons/vue/24/solid";
+
+const { isDark, toggleDark } = useTheme();
+</script>
+
+<template>
+  <Button @click="toggleDark()">
+    <MoonIcon v-if="!isDark" class="w-6 h-6 text-indigo-500" />
+    <SunIcon v-else class="w-6 h-6 dark:text-indigo-200" />
+  </Button>
+</template>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -2,5 +2,9 @@
   <div>
     Some default layout shared across all pages
     <slot />
+
+    <div>
+      <ToggleDarkButton />
+    </div>
   </div>
 </template>
