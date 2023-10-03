2fca33c918bd2d0ff412fc532af5d4c22a5bb6fb
AirFast <airfast.88@gmail.com>
2023-10-03 14:09:31 +0300

    feat: add price component

---
+++ frontend/components/Price.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <p class="flex gap-2 items-baseline font-bold text-lg text-green-500">
+      $24.00
+      <span class="font-semibold text-base text-slate-500 line-through">
+        $28.00
+      </span>
+    </p>
+    <p class="text-sm font-semibold text-green-500">
+      You save: $28.00 (50%)
+    </p>
+  </div>
+</template>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -7,6 +7,7 @@
     </div>
     <div class="grid gap-6 md:gap-7 md:text-lg">
       <h1 class="text-4xl font-bold">Home page</h1>
+      <Price />
       <div class="flex gap-4">
         <Button variant="action">Add to cart</Button>
         <Button>Save</Button>
@@ -28,88 +29,6 @@
         bibendum finibus. Praesent pharetra purus eget augue suscipit viverra.
         Ut lectus magna, vestibulum vulputate malesuada id, faucibus eu felis.
         Sed scelerisque nec mauris viverra facilisis.
-      </p>
-      <p>
-        Maecenas nec sodales tellus. Suspendisse hendrerit libero vitae felis
-        laoreet sodales. Etiam consectetur arcu id velit maximus, a lobortis
-        tortor vehicula. Etiam nec posuere turpis. Suspendisse arcu justo,
-        euismod id suscipit vitae, congue ut justo. Nulla facilisi. Nam lobortis
-        purus et elit volutpat molestie. In vel lacus fringilla, molestie mauris
-        vitae, accumsan justo.
-      </p>
-      <p>
-        In ut ornare est. Aliquam erat volutpat. In ut molestie felis. Quisque
-        gravida, dolor vitae gravida condimentum, mi tortor viverra enim, et
-        tempor magna orci at libero. Mauris et lacinia odio, sit amet laoreet
-        leo. Quisque sed vehicula justo, at viverra augue. Donec viverra congue
-        nisl sit amet maximus. Aenean tellus lorem, tincidunt sit amet porta
-        non, elementum id velit. Donec semper laoreet quam semper bibendum. In
-        libero magna, mattis vel aliquam sit amet, blandit vel arcu. Praesent
-        vel vestibulum orci.
-      </p>
-      <p>
-        Aliquam feugiat rutrum turpis ut congue. Vestibulum sem enim, aliquam
-        quis gravida nec, venenatis quis tortor. Aliquam maximus, nisi id
-        finibus sollicitudin, magna velit sodales metus, ac vehicula sem arcu ut
-        est. Etiam porttitor nisi in elementum fermentum. Donec nisi nisl,
-        congue ut vulputate eget, vestibulum et nisl. Curabitur sit amet dolor
-        nulla. Vivamus lacinia magna ac quam luctus, at luctus massa faucibus.
-        Fusce ut venenatis massa. Etiam vitae leo vel elit faucibus auctor eget
-        blandit quam. Vestibulum dignissim neque quis enim ultrices, accumsan
-        accumsan turpis efficitur. Vestibulum commodo sagittis tortor, sed
-        condimentum nisi maximus fringilla. Duis in dolor vitae ante viverra
-        luctus. Aliquam a egestas ex, vehicula rhoncus orci. In sit amet massa
-        ac lectus ullamcorper accumsan.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
-        euismod mi. Curabitur at laoreet sem. Fusce convallis elit leo, id
-        varius risus eleifend id. Quisque at quam elit. Morbi bibendum et orci
-        at porta. Duis eget tellus enim. Nulla sodales et nibh nec aliquam.
-      </p>
-      <p>
-        Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Mauris augue lacus, tristique eu porta non, luctus eget
-        quam. Sed eget nunc at felis vulputate posuere non vel augue. Sed nisl
-        magna, auctor vel eleifend at, egestas et justo. Pellentesque a turpis
-        sit amet massa varius posuere. Vivamus eu turpis ante. Fusce volutpat
-        sem a magna elementum lobortis. Phasellus dui felis, rutrum vitae dolor
-        aliquet, fringilla placerat nisl. Nullam sodales tortor sed elit
-        bibendum finibus. Praesent pharetra purus eget augue suscipit viverra.
-        Ut lectus magna, vestibulum vulputate malesuada id, faucibus eu felis.
-        Sed scelerisque nec mauris viverra facilisis.
-      </p>
-      <p>
-        Maecenas nec sodales tellus. Suspendisse hendrerit libero vitae felis
-        laoreet sodales. Etiam consectetur arcu id velit maximus, a lobortis
-        tortor vehicula. Etiam nec posuere turpis. Suspendisse arcu justo,
-        euismod id suscipit vitae, congue ut justo. Nulla facilisi. Nam lobortis
-        purus et elit volutpat molestie. In vel lacus fringilla, molestie mauris
-        vitae, accumsan justo.
-      </p>
-      <p>
-        In ut ornare est. Aliquam erat volutpat. In ut molestie felis. Quisque
-        gravida, dolor vitae gravida condimentum, mi tortor viverra enim, et
-        tempor magna orci at libero. Mauris et lacinia odio, sit amet laoreet
-        leo. Quisque sed vehicula justo, at viverra augue. Donec viverra congue
-        nisl sit amet maximus. Aenean tellus lorem, tincidunt sit amet porta
-        non, elementum id velit. Donec semper laoreet quam semper bibendum. In
-        libero magna, mattis vel aliquam sit amet, blandit vel arcu. Praesent
-        vel vestibulum orci.
-      </p>
-      <p>
-        Aliquam feugiat rutrum turpis ut congue. Vestibulum sem enim, aliquam
-        quis gravida nec, venenatis quis tortor. Aliquam maximus, nisi id
-        finibus sollicitudin, magna velit sodales metus, ac vehicula sem arcu ut
-        est. Etiam porttitor nisi in elementum fermentum. Donec nisi nisl,
-        congue ut vulputate eget, vestibulum et nisl. Curabitur sit amet dolor
-        nulla. Vivamus lacinia magna ac quam luctus, at luctus massa faucibus.
-        Fusce ut venenatis massa. Etiam vitae leo vel elit faucibus auctor eget
-        blandit quam. Vestibulum dignissim neque quis enim ultrices, accumsan
-        accumsan turpis efficitur. Vestibulum commodo sagittis tortor, sed
-        condimentum nisi maximus fringilla. Duis in dolor vitae ante viverra
-        luctus. Aliquam a egestas ex, vehicula rhoncus orci. In sit amet massa
-        ac lectus ullamcorper accumsan.
       </p>
     </div>
   </div>
